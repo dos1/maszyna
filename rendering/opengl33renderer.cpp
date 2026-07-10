15f93a00dd4be81a7218971bdc7a485f113c3bb9
Sebastian Krzyszkowiak <dos@dosowisko.net>
2026-07-10 07:03:20 +0200

    Revert "opengl33renderer: Adjust sun intensity based on overcast"
    This reverts commit f2a7cc4f98b0d0a249532d92d87c76d5fd88136b.

--- rendering/opengl33renderer.cpp
+++ rendering/opengl33renderer.cpp
@@ -1924,7 +1924,7 @@ void opengl33_renderer::setup_environment_light(TEnvironmentType const Environme
 void opengl33_renderer::setup_sunlight_intensity( float const Factor ) {
 
     m_sunlight.apply_intensity( Factor );
-    light_ubs.lights[ 0 ].intensity = m_sunlight.factor * (1.0f - Global.Overcast * 0.33f);
+    light_ubs.lights[ 0 ].intensity = m_sunlight.factor;
     light_ubs.ambient = m_sunlight.ambient * m_sunlight.factor;
     light_ubo->update( light_ubs );
 }
